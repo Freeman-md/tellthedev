217667dd3f311d5d35f5d6a46c5b5b143197f9ac
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-06-20 08:10:13 +0100

    feat: implement useLoginForm composable for handling login form state and validation

---
+++ composables/useLoginForm.ts
@@ -0,0 +1,75 @@
+import { ref, reactive } from 'vue'
+import { useSupabaseClient, useToast } from '#imports'
+
+export const useLoginForm = () => {
+  const isSubmitting = ref(false)
+
+  const form = reactive({
+    email: '',
+    password: '',
+  })
+
+  const errors = reactive({
+    email: '',
+    password: '',
+  })
+
+  const toast = useToast()
+  const supabase = useSupabaseClient()
+
+  const validateForm = () => {
+    let isValid = true
+
+    errors.email = ''
+    errors.password = ''
+
+    if (!form.email.includes('@')) {
+      errors.email = 'Please enter a valid email'
+      isValid = false
+    }
+
+    if (!form.password) {
+      errors.password = 'Password is required'
+      isValid = false
+    }
+
+    return isValid
+  }
+
+  const handleSubmit = async () => {
+    if (!validateForm()) return
+
+    isSubmitting.value = true
+
+    try {
+      const { error } = await supabase.auth.signInWithPassword({
+        email: form.email,
+        password: form.password,
+      })
+
+      if (error) {
+        toast.add({ title: error.message, color: 'error' })
+        return
+      }
+
+      toast.add({
+        title: '✅ Login successful',
+      })
+
+    } catch (err) {
+      toast.add({
+        title: `Unexpected error: ${err}`,
+        color: 'error',
+      })
+    } finally {
+      isSubmitting.value = false
+    }
+  }
+
+  return {
+    form,
+    errors,
+    isSubmitting,
+    handleSubmit,
+  }
+}
--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,7 +1,62 @@
+<script setup lang="ts">
+import { definePageMeta, useLoginForm } from '#imports'
+import AuthInput from '~/components/auth/AuthInput.vue'
+
+definePageMeta({
+  layout: 'auth',
+})
+
+const { form, errors, isSubmitting, handleSubmit } = useLoginForm()
+</script>
+
 <template>
+  <div class="space-y-8 lg:max-w-4/5">
+    <section id="header" class="space-y-2">
+      <h1 class="text-3xl font-semibold">Welcome back,</h1>
+      <p class="text-gray-700">Sign in to continue</p>
+    </section>
 
-    <div class="bg-red-400">
-        This is the login page
-    </div>
+    <form id="form" class="space-y-6">
+      <AuthInput
+        id="email"
+        label="Email"
+        type="email"
+        placeholder="Email"
+        v-model="form.email"
+        :error="errors.email"
+      />
+      <AuthInput
+        id="password"
+        label="Password"
+        type="password"
+        placeholder="Password"
+        v-model="form.password"
+        :error="errors.password"
+      />
 
+      <div class="mt-4 space-y-4">
+        <UButton
+          :loading="isSubmitting"
+          class="w-full text-white justify-center !text-center py-2 cursor-pointer"
+          @click="handleSubmit"
+        >
+          Sign in
+        </UButton>
+        <UButton
+          icon="devicon:google"
+          color="neutral"
+          class="w-full justify-center border border-gray-200 cursor-pointer py-2"
+        >
+          Continue with Google
+        </UButton>
+      </div>
+    </form>
+
+    <section id="footer">
+      Don’t have an account?
+      <NuxtLink class="text-primary hover:underline" to="/auth/register">
+        Create one
+      </NuxtLink>
+    </section>
+  </div>
 </template>
