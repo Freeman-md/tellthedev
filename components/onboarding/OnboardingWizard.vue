cbeae3d352ebd7502b760b99b161a7fdfa04fc12
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-06-23 19:18:45 +0100

    feat: add slot definitions for steps in OnboardingWizard component

--- components/onboarding/OnboardingWizard.vue
+++ components/onboarding/OnboardingWizard.vue
@@ -20,21 +20,25 @@ const formData = ref({
 
 const steps = ref<StepperItem[]>([
   {
+    slot: "project-info" as const,
     title: "Project Info",
     description: "Name and describe your project",
     icon: "i-lucide-folder-plus",
   },
   {
+    slot: "allowed-origins" as const,
     title: "Allowed Origins",
     description: "Add your allowed domain(s)",
     icon: "i-lucide-globe",
   },
   {
+    slot: "widget-settings" as const,
     title: "Widget Settings",
     description: "Configure look and behavior",
     icon: "i-lucide-sliders-horizontal",
   },
   {
+    slot: "finish-setup" as const,
     title: "Finish Setup",
     description: "Grab install code and preview",
     icon: "i-lucide-check-circle",
@@ -54,6 +58,20 @@ const stepper = useTemplateRef<UStepperRef>("stepper");
       :items="steps"
       class="w-full"
     >
+
+    <template #project-info>
+      <div class="aspect-video">This is for Project Info</div>
+    </template>
+    <template #allowed-origins>
+      <div class="aspect-video">This is for Allowed Origins</div>
+    </template>
+    <template #widget-settings>
+      <div class="aspect-video">This is for Widget Settings</div>
+    </template>
+    <template #finish-setup>
+      <div class="aspect-video">This is for Finish Setup</div>
+    </template>
+
     </UStepper>
 
     <div class="flex gap-2 justify-between mt-4">
