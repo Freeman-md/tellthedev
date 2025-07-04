b73145c42bf30f8f937a92b7cddb47ab56aee5fa
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-07-04 10:54:56 +0100

    refactor: remove landing-only middleware as it is no longer needed

--- middleware/00.landing-only.global.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware((to) => {
-  if (process.env.NODE_ENV !== 'production') return
-  if (to.path !== '/') {
-    return navigateTo('/', { replace: true })
-  }
-}) 
