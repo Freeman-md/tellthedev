9ab6ebe86b25b452dc137d0e5c0db82905caed53
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-07-05 07:35:46 +0100

    feat: add @vercel/analytics dependency and integrate Analytics component

--- app.vue
+++ app.vue
@@ -1,8 +1,13 @@
 <template>
   <UApp>
     <NuxtLayout>
+      <Analytics />
       <NuxtRouteAnnouncer />
       <NuxtPage />
     </NuxtLayout>
   </UApp>
 </template>
+
+<script lang="ts" setup>
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>
